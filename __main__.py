c776a9b0ebcd0459dc24ffdea856782a49c77ded
DSDEV27001 <michael@33n.co.uk>
2020-04-26 00:15:16 +0100

    Started Transformation Process

--- __main__.py
+++ __main__.py
@@ -2,60 +2,13 @@ import pandas as pd
 import numpy as np
 import logging
 import functools
-from pandas_schema import Column, Schema
-from pandas_schema.validation import (
-    MatchesPatternValidation,
-    InRangeValidation,
-    IsDtypeValidation,
-)
+from pandas_schema import Column, Schema, validation as val
 
-FILENAMES = ["Data\weather.20160201", "Data\weather.20160301"]
+FILENAMES = ["weather.20160201", "weather.20160301"]
 
 logger = logging.getLogger(__name__)
 fh = logging.FileHandler("error.log")
 logger.addHandler(fh)
-
-weather_file_schema = Schema(
-    [
-        Column("ForecastSiteCode", [IsDtypeValidation(np.dtype(int))]),
-        Column(
-            "ObservationTime",
-            [InRangeValidation(0, 23), IsDtypeValidation(np.dtype(int))],
-        ),
-        Column("ObservationDate"),
-        Column(
-            "WindDirection",
-            [InRangeValidation(0, 16), IsDtypeValidation(np.dtype(int))],
-        ),  # 16 points of the compass (N is 0 and 16 since it is 0 and 360 degrees)
-        Column("WindSpeed", [IsDtypeValidation(pd.Int64Dtype())], allow_empty=True),
-        Column("WindGust", [IsDtypeValidation(pd.Int64Dtype())], allow_empty=True),
-        Column("Visibility", [IsDtypeValidation(pd.Int64Dtype())], allow_empty=True),
-        Column(
-            "ScreenTemperature",
-            [InRangeValidation(-50, +50), IsDtypeValidation(np.dtype(float))],
-            allow_empty=True,
-        ),
-        Column("Pressure", [IsDtypeValidation(pd.Int64Dtype())], allow_empty=True),
-        Column("SignificantWeatherCode", [InRangeValidation(0, 30)], allow_empty=True),
-        Column("SiteName", [IsDtypeValidation(np.dtype(str))],),
-        Column(
-            "Latitude", [InRangeValidation(-90, 90), IsDtypeValidation(np.dtype(float))]
-        ),  # Signed latitude range
-        Column(
-            "Longitude",
-            [InRangeValidation(-180, 80), IsDtypeValidation(np.dtype(float))],
-        ),  # Signed longtitude range
-        Column("Region", [IsDtypeValidation(np.dtype(str))], allow_empty=True),
-        Column(
-            "Country",
-            [
-                MatchesPatternValidation(r"\d{4}[A-Z]{4}"),
-                IsDtypeValidation(np.dtype(str)),
-            ],
-            allow_empty=True,
-        ),
-    ]
-)
 
 
 def log_error(logger):
@@ -86,13 +39,76 @@ def import_monthly_weather_csv(fpath: str) -> pd.DataFrame:
     Removes leading and trailing whitespace
     """
     frame_out = pd.read_csv(
-        f"{fpath}.csv", na_values=-99, float_precision="round_trip"
+        f"Data/{fpath}.csv", na_values=-99, float_precision="round_trip"
     ).applymap(lambda x: x.strip() if isinstance(x, str) else x)
     return frame_out
 
 
-def validate_weather_df(frame_in: pd.DataFrame) -> pd.DataFrame:
-    return frame_in
+def validate_weather_data(frame_in: pd.DataFrame):
+    # TODO Min only range and length
+    weather_file_schema = Schema(
+        [
+            Column("ForecastSiteCode", [val.IsDtypeValidation(np.dtype("int64"))]),
+            Column(
+                "ObservationTime",
+                [
+                    val.InRangeValidation(0, 24),
+                    val.IsDtypeValidation(np.dtype("int64")),
+                ],
+            ),
+            Column("ObservationDate", [val.DateFormatValidation("%Y-%m-%dT%H:%M:%S")]),
+            Column(
+                "WindDirection",
+                [
+                    val.InRangeValidation(0, 17),
+                    val.IsDtypeValidation(np.dtype("int64")),
+                ],
+            ),  # 16 points of the compass (N is 0 and 16 since it is 0 and 360 degrees)
+            Column("WindSpeed", [val.CanConvertValidation(int)], allow_empty=True),
+            Column("WindGust", [val.CanConvertValidation(int)], allow_empty=True),
+            Column("Visibility", [val.CanConvertValidation(int)], allow_empty=True),
+            Column(
+                "ScreenTemperature",
+                [
+                    val.InRangeValidation(-50, +50),
+                    val.IsDtypeValidation(np.dtype(float)),
+                ],
+                allow_empty=True,
+            ),
+            Column("Pressure", [val.CanConvertValidation(int)], allow_empty=True),
+            Column(
+                "SignificantWeatherCode",
+                [val.InRangeValidation(0, 31)],
+                allow_empty=True,
+            ),
+            Column("SiteName", [val.CanConvertValidation(str)],),
+            Column(
+                "Latitude",
+                [
+                    val.InRangeValidation(-90, 90),
+                    val.IsDtypeValidation(np.dtype(float)),
+                ],
+            ),  # Signed latitude range
+            Column(
+                "Longitude",
+                [
+                    val.InRangeValidation(-180, 80),
+                    val.IsDtypeValidation(np.dtype(float)),
+                ],
+            ),  # Signed longitude range
+            Column("Region", [val.CanConvertValidation(str)], allow_empty=True),
+            Column("Country", [val.CanConvertValidation(str)], allow_empty=True),
+        ]
+    )
+
+    errors = weather_file_schema.validate(frame_in)
+    if len(errors) > 0:
+        for error in errors:
+            logger.exception(error)
+        # TODO: Create exception class to handle error
+        raise Exception(
+            "Error: Weather files data validation failed. Please refer to the log file for detailed information."
+        )
 
 
 def transform_weather_df(frame_in: pd.DataFrame) -> pd.DataFrame:
@@ -103,21 +119,36 @@ def transform_weather_df(frame_in: pd.DataFrame) -> pd.DataFrame:
     Use reverse geocode generated data to populate Region and Country with more accurate data
     Remove duplicates
     """
-    return frame_in
+    frame_out = frame_in.assign(
+        ObservationTime = lambda df: df.ObservationTime.astype(np.dtype(str)),
+        ObservationDateTime=lambda df: df.ObservationDate.str.slice(0, 11)
+        + df.ObservationTime.str.zfill(2)
+        + df.ObservationDate.str.slice(13,)
+    )
+    return frame_out
 
 
 def export_weather_to_parquet(frame_in: pd.DataFrame):
-    frame_in.to_parquet()
+    frame_in.to_parquet("Data/weather.parquet", compression="GZIP")
 
 
 def main():
     raw_weather_frame = pd.concat(
         import_monthly_weather_csv(filename) for filename in FILENAMES
     )
-    export_weather_to_parquet(raw_weather_frame)
+
+    validate_weather_data(raw_weather_frame)
+
+    weather_frame_out = transform_weather_df(raw_weather_frame)
+    export_weather_to_parquet(weather_frame_out)
 
 
-# main()
+main()
 
 
-frame = import_monthly_weather_csv(f"Data\weather.20160201")
+# frame = import_monthly_weather_csv("weather.20160201")
+#
+# errors = weather_file_schema.validate(frame)
+#
+# for error in errors:
+#     print(error)
